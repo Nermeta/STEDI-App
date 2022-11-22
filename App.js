a8fd3da798cd6f5c65a328a8e7967a6eaec5f096
Nermeta <56845892+Nermeta@users.noreply.github.com>
2022-11-22 13:08:11 -0700

    Camera
    Caught up to what I missed

--- App.js
+++ App.js
@@ -6,49 +6,61 @@ import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-
-
 const AppStack = createNativeStackNavigator();
-
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
-  const [phoneNumber, setPhoneNumber] = React.useState("");
   const [isLoggedIn,setIsLoggedIn] = React.useState(false);
+  const [phoneNumber, setPhoneNumber] = React.useState("");
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
-  const [tempCode, setTempCode] = React.useState(null);
-
-   if (isFirstLaunch == true){
+  const [tempCode , setTempCode] = React.useState(null);
+  
+  useEffect(()=>{
+    const getSessionToken = async() => {
+      const sessionToken = await AsyncStorage.getItem('sessionToken');
+      console.log('Token from Storage', sessionToken);
+      const validateResponse = await fetch('https://dev.stedi.me/validate/'+ sessionToken);
+      
+      if(validateResponse.status == 200){
+        const userEmail = await validateResponse.text();
+        console.log('userEmail', userEmail);
+        setIsLoggedIn(true);
+      }
+    }
+    getSessionToken();
+  },[])
+   if (isFirstLaunch == true &&! isLoggedIn){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
- 
 );
   }else if(isLoggedIn){
     return <Navigation/>
   } else{
-    return(
+    return (
       <View>
-        <TextInput 
-        value={phoneNumber}
-        onChangeText={setPhoneNumber}
-        style={styles.input}
-        placeholderTextColor = '#4251f5'
-        placeholder = 'Cell Phone'>
+        <TextInput
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+          style={styles.input}
+          placeholderTextColor='#4251F5'
+          placeholder='Cell Phone'>
         </TextInput>
         <Button
-        title='Send'
-        sytle={styles.button}
-        onPress={async()=>{
-          console.log(phoneNumber)
-
-          await fetch(
-            'https://dev.stedi.me/twofactorlogin/' + phoneNumber,
-            {
-              method:'POST',
-              headers:{
-                'content-type' : 'application/text'
+          title='Send'
+          style={styles.button}
+          onPress={async()=>{
+            console.log('Button was pressed')
+            const sendTextResponse=await fetch(
+              'https://dev.stedi.me/twofactorlogin/'+ phoneNumber,
+              {
+                method: 'POST',
+                headers:{
+                  'content-type' : 'application/text'
+                }
               }
+            )
+            if (sendTextResponse.status!=200){
+              console.log('Server send text response: '+sendTextResponse.status);
+              Alert('Communication Error','Server responded to send text with status: '+sendTextResponse.status);
             }
           ).catch((err) => {console.log(err)})
         }}
@@ -81,8 +93,9 @@ return(
         )
         if (loginResponse.status==200){
           const sessionToken = await loginResponse.text();
-          await AsyncStorage.setItem('sessionToken', sessionToken)
+          await AsyncStorage.setItem('sessionToken', sessionToken);
           console.log('Session Token', sessionToken);
+
           setIsLoggedIn(true);
         } else{
           console.log("Token response Status",loginResponse.status)
@@ -95,7 +108,6 @@ return(
   }
 }
  export default App;
-
  const styles = StyleSheet.create({
   container:{
       flex:1, 
